dc8f5a6e2d613807e98a79def9a4e97b4f2c6a49
Leandro Pinheiro <82143616+leandropinheiroalves@users.noreply.github.com>
2021-09-11 15:55:21 -0300

    Criada classe Homem herdando atributos da classe Pessoa

--- python/pythonbirds/fase.py
+++ python/pythonbirds/fase.py
@@ -2,7 +2,6 @@
 from itertools import chain
 from atores import ATIVO
 
-
 VITORIA = 'VITORIA'
 DERROTA = 'DERROTA'
 EM_ANDAMENTO = 'EM_ANDAMENTO'
@@ -35,7 +34,6 @@ class Fase():
         self._passaros = []
         self._porcos = []
         self._obstaculos = []
-
 
     def adicionar_obstaculo(self, *obstaculos):
         """
@@ -88,7 +86,6 @@ class Fase():
         """
         pass
 
-
     def calcular_pontos(self, tempo):
         """
         Lógica que retorna os pontos a serem exibidos na tela.
@@ -98,10 +95,9 @@ class Fase():
         :param tempo: tempo para o qual devem ser calculados os pontos
         :return: objeto do tipo Ponto
         """
-        pontos=[self._transformar_em_ponto(a) for a in self._passaros+self._obstaculos+self._porcos]
+        pontos = [self._transformar_em_ponto(a) for a in self._passaros + self._obstaculos + self._porcos]
 
         return pontos
 
     def _transformar_em_ponto(self, ator):
         return Ponto(ator.x, ator.y, ator.caracter())
-
--- python/pythonbirds/oo/pessoa.py
+++ python/pythonbirds/oo/pessoa.py
@@ -17,9 +17,14 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+
+class Homem(Pessoa):
+    pass
+
+
 if __name__ == '__main__':
-    renzo = Pessoa(nome='Renzo')
-    luciano = Pessoa(renzo, nome='Luciano')
+    renzo = Homem(nome='Renzo')
+    luciano = Homem(renzo, nome='Luciano')
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
@@ -40,3 +45,10 @@ if __name__ == '__main__':
     print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
     print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
+    pessoa = Pessoa('Anônimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(renzo, Pessoa))
+    print(isinstance(renzo, Homem))
+    print(isinstance(luciano, Pessoa))
+    print(isinstance(luciano, Homem))
