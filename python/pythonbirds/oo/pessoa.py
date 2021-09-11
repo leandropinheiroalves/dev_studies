c6e406c82d5f046daec5af1bf6253e3387734735
Leandro Pinheiro <82143616+leandropinheiroalves@users.noreply.github.com>
2021-09-11 13:05:48 -0300

    Criado atributo de classe olhos

--- python/pythonbirds/oo/pessoa.py
+++ python/pythonbirds/oo/pessoa.py
@@ -1,8 +1,10 @@
 class Pessoa:
+    olhos = 2  # atributo de classe
+
     def __init__(self, *filhos, nome=None, idade=35):
-        self.idade = idade
-        self.nome = nome
-        self.filhos = list(filhos)
+        self.idade = idade  # atributo de instância
+        self.nome = nome  # atributo de instância
+        self.filhos = list(filhos)  # atributo de instância
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
@@ -14,10 +16,18 @@ if __name__ == '__main__':
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
+    print(luciano.nome)
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
     luciano.sobrenome = 'Ramalho'
     del luciano.filhos
+    luciano.olhos = 1
+    del luciano.olhos
     print(luciano.__dict__)
     print(renzo.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(renzo.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
