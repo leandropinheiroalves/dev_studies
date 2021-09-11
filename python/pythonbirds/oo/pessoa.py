c06a204f3c27e135c6c03e5a513b488fae8c33ad
Leandro Pinheiro <82143616+leandropinheiroalves@users.noreply.github.com>
2021-09-11 12:11:46 -0300

    Criado atributo complexo filhos

--- python/pythonbirds/oo/pessoa.py
+++ python/pythonbirds/oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    p.nome = 'Renzo'
-    print(p.nome)
-    print(p.idade)
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
