84de1ca8ed2790801529b2340f5a1241f84072e5
Leandro <leandropinheiroalves@gmail.com>
2021-10-06 18:17:36 -0300

    Update ex061.py

--- python/mundo_1_2_e_3_cursoemvideo_gustavo_guanabara/ex061.py
+++ python/mundo_1_2_e_3_cursoemvideo_gustavo_guanabara/ex061.py
@@ -1,11 +1,12 @@
 """
 Refaça o exercício 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.
 """
-termo = int(input('Digite o valor do primeiro termo: '))
-razao = int(input('Digite o valor da razao: '))
-contador = 0
-while contador < 10:
-    print(f'{termo}', end=' → ')
+
+termo = int(input('Digite o valor do primeiro termo: '))  # Recebendo primeiro valor
+razao = int(input('Digite o valor da razao: '))  # Recebendo segundo valor
+contador = 0  # Criando contador
+while contador < 10:  # Loop ENQUANTO o contador for menor que 10
+    print(f'{termo}', end=' → ')  
     termo += razao
     contador += 1
 print('FIM')
