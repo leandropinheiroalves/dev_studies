6053362c75d1dd6597edc9d5cd8a34d8ef6a6a2e
Leandro Alves <leandropinheiroalves@gmail.com>
2024-09-09 08:38:15 -0400

    resolvido exercício dia 02 operators

---
+++ python/hacker_rank/30_days_of_code/day-02-operators.py
@@ -0,0 +1,34 @@
+'''https://www.hackerrank.com/challenges/30-operators?isFullScreen=true'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'solve' function below.
+#
+# The function accepts following parameters:
+#  1. DOUBLE meal_cost
+#  2. INTEGER tip_percent
+#  3. INTEGER tax_percent
+#
+
+def solve(meal_cost, tip_percent, tax_percent):
+    # Write your code here
+    tip_percent = meal_cost * tip_percent / 100 
+    tax_percent = meal_cost * tax_percent / 100
+    resultado = meal_cost + tip_percent + tax_percent
+    print(round(resultado))
+    
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+
+    tip_percent = int(input().strip())
+
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
