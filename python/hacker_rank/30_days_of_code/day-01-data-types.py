87a55caade2d71dd37783ae8d151bbee445e8052
Leandro Alves <leandropinheiroalves@gmail.com>
2024-09-09 08:30:59 -0400

    resolvido exercício dia 01 data types

---
+++ python/hacker_rank/30_days_of_code/day-01-data-types.py
@@ -0,0 +1,17 @@
+'''https://www.hackerrank.com/challenges/30-data-types/problem?isFullScreen=true'''
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+inteiro = int(input())
+real = float(input())
+frase = input()
+# Print the sum of both integer variables on a new line.
+print(i + inteiro)
+# Print the sum of the double variables on a new line.
+print(d + real)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + frase)
