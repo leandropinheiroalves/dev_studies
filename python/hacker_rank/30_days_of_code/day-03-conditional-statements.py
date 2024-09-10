3aa92f6b73e42d23b27ddb33f9267cb913eb5c1f
Leandro Alves <leandropinheiroalves@gmail.com>
2024-09-10 11:04:15 -0400

    resolvido exercício dia-03-conditional-statements

---
+++ python/hacker_rank/30_days_of_code/day-03-conditional-statements.py
@@ -0,0 +1,23 @@
+'''https://www.hackerrank.com/challenges/30-conditional-statements/problem?isFullScreen=true'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    if N % 2 != 0:
+        print('Weird')
+    else:
+        if N >= 2 and N < 5:
+            print('Not Weird')
+        elif N >= 6 and N <= 20:
+            print('Weird')
+        else:
+            print('Not Weird')
