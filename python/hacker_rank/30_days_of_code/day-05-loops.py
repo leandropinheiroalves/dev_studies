d093bbad9961e8968fcb5688c2d3d08544dfa542
Leandro Alves <leandropinheiroalves@gmail.com>
2024-09-10 11:19:57 -0400

    resolvido exercício dia-05-loops

---
+++ python/hacker_rank/30_days_of_code/day-05-loops.py
@@ -0,0 +1,17 @@
+'''https://www.hackerrank.com/challenges/30-loops/problem?isFullScreen=true'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    for i in range(1, 11):
+        print(f'{n} x {i} = {n * i}')
+        
