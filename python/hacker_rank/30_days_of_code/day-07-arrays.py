cdcee4fcd55ec85978709c51b536ab3f3d13b4e9
Leandro Alves <leandropinheiroalves@gmail.com>
2024-09-10 11:23:27 -0400

    resolvido exercício dia-07-arrays

---
+++ python/hacker_rank/30_days_of_code/day-07-arrays.py
@@ -0,0 +1,18 @@
+'''https://www.hackerrank.com/challenges/30-arrays/problem?isFullScreen=true'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+print(*arr[::-1])
